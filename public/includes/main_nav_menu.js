693a54832394277aec4b505c6a03ef9a8b655727
thezevnews@gmail.com <thezevnews@gmail.com>
2020-01-07 00:28:38 -0300

    Menus superior e inferior incluidos e all e news com links corrigidos

--- public/includes/main_nav_menu.js
+++ public/includes/main_nav_menu.js
@@ -3,27 +3,7 @@
 function activate_main_menu() {
 	
 		
-activate_sub_menu();
 
-
- document.getElementById("myTopnav").innerHTML = '<a href="/home"    id="home_item">HOME</a>'+ 
-												'<a href="/carros"   id="carros_item">CARROS</a>'+
-												' <a href="/motos"   id="motos_item">MOTOS</a>'+
-												'<a href="/bikes"    id="bikes_item">BIKES</a>'+
-												'<a href="/tech"     id="tech_item"">TECH</a>'+
-												'<a href="/negocios" id="negocios_item">NEGÓCIOS</a>'+
-												'<a href="/racing"   id="racing_item">RACING</a>'+
-												'<a href="/startups" id="startups_item">STARTUPS</a>'+
-												'<a href="http://www.zev.news/contato"  id="contato_item">CONTATO</a>'+
-												'<a href="#about"><img src="icons/facebooklogo.png"  class="logo_social"></a>'+
-												'<a href="#about"><img src="icons/twitterlogo.png"   class="logo_social"></a>'+
-												'<a href="#about"><img src="icons/instagramlogo.png" class="logo_social"></a>'+
-												'<a href="javascript:void(0);" class="icon" onclick="myFunction()">'+
-												'<i class="fa fa-bars"></i>'+
-												'</a>'
-												
- 
- 
  
   /*var page_section = document.getElementById("page_section");*/
  
@@ -102,13 +82,3 @@ activate_sub_menu();
  
 }
 
-function activate_sub_menu(){
-	
-	document.getElementById("menu_footer").innerHTML = '<ul>'+
-														'<li><a href="https://zev.news/termo_de_uso_zevnews.html">Termos de Uso</a></li>'+
-			                                            '<li><a href="https://zev.news/politica_de_privacidade_zevnews.html">Política de Privacidade</a></li>'+
-			                                            '<li><a href="https://zev.news/sobre_zevnews.html">Sobre Zev.News</a></li>'+
-			                                            '<li><a href="https://zev.news/contato.html">Entre em Contato</a></li>'+
-		                                                '</ul>'
-	
-}
